30848ddcf8eb1059ece574161caf7e24b3fef3b2
Doston12 <rajabovdost@gmail.com>
2025-03-16 14:16:31 +0500

    add

--- calculator.py
+++ calculator.py
@@ -3,4 +3,7 @@ from tkinter import Tk, Entry, Button, StringVar
 class Calculator:
     def__init__(self,master):
     master.title("Calculator")  
+    master.geometry('357x420+0+0')
+    master.config(bg='gray')
+    master.resizable(False,False)
     
